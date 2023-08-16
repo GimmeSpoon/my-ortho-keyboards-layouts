573767b48f36a9752f1bff8d40d9902a18b25bc8
GimmeSpoon <skcjfdnd1996@gmail.com>
2023-08-16 21:49:08 +0900

    added new config files

--- myplanck/keymap.c
+++ myplanck/keymap.c
@@ -102,16 +102,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ScrLck|  P-  |  P+  |Ps/Brk|      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | Mute |  P-  |  P+  | Play |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |Hanja |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |Hanja |      |             |      | Prev | Vol- | Vol+ | Next |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_SCRL, KC_PMNS, KC_PPLS, KC_PAUS, _______,
-    _______, _______, _______, KC_LNG2, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_MUTE, KC_PMNS, KC_PPLS, KC_MPLY, _______,
+    _______, _______, _______, KC_LNG2, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
 ),
 
 /* Raise
@@ -120,15 +120,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |PrtSc |ISO #~|ISO /||Insert|      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |PrtScr|Insert|ScrLck| Paus |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Home | Pg Dn| Pg Up| End  |
+ * |      |      |      |Hanja |      |             |      | Home | Pg Dn| Pg Up| End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_NUHS, KC_NUBS, KC_INS,  _______,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_INS,  KC_SCRL, KC_PAUS, _______,
     _______, _______, _______, KC_LNG2, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
@@ -153,19 +153,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Adjust (Lower + Raise)
  *                      v------------------------RGB CONTROL--------------------v
  * ,-----------------------------------------------------------------------------------.
- * |      |Qwerty|Colemk|Dvorak| Reset|Debug | RGB  |RGBMOD|TermOn|TermOf|Plover|  Del |
+ * |      |Qwerty|Colemk|Dvorak| Reset|Debug | RGB  |RGBMOD|      |      |Plover|  Del |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |Aud on|Audoff|MUSmod|AGnorm|AGswap| HUE+ | HUE- | SAT+ | SAT- |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |BRGTH+|BRGTH-|Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |      |      |
+ * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |BRGTH-|BRGTH+|      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Brite|      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-    _______, QWERTY, COLEMAK,   DVORAK, QK_BOOT,   DEBUG, RGB_TOG, RGB_MOD, TERM_ON,TERM_OFF, PLOVER,  KC_DEL ,
+    _______,  QWERTY, COLEMAK,  DVORAK, QK_BOOT,   DEBUG, RGB_TOG, RGB_MOD, _______, _______, PLOVER,  KC_DEL ,
     _______,   AU_ON,  AU_OFF,  MU_MOD, AG_NORM, AG_SWAP, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, _______, _______,
-    _______, RGB_VAI, RGB_VAD,  MUV_DE,  MUV_IN,   MU_ON,  MU_OFF,   MI_ON,  MI_OFF, _______, _______, _______,
+    _______,  MUV_DE,  MUV_IN,   MU_ON,  MU_OFF,   MI_ON,  MI_OFF, _______, RGB_VAD, RGB_VAI, _______, _______,
     BACKLIT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
